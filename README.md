06a5e3d6bddbb97479114cf62afc07ad64b93100
eymea <eduardo@eymea.com>
2025-03-10 21:06:40 +1100

    v2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pyTALIB-Test
 Sample Code to test TA-Lib
+
+## Dependency
+pip install ta_lib-0.6.3-cp312-cp312-win_amd64.whl
---
+++ src/lambda_function.py
@@ -0,0 +1,34 @@
+import json
+import numpy as np
+import talib as ta
+
+def lambda_handler(event, context):
+
+    close = np.random.random(100)
+    print("****** Random values x 100 ******")
+    print(close)
+
+    output = ta.SMA(close)
+    print("****** SMA ******")
+    print(output)
+
+    # list of functions
+    print("****** Functions ******")
+    for name in ta.get_functions():
+        print(name)
+
+    # dict of functions by group
+    print("****** Functions by group ******")
+    for group, names in ta.get_function_groups().items():
+        print(group)
+        for name in names:
+            print(f"  {name}")
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda! v2')
+    }
+
+if __name__ == "__main__":
+    print(lambda_handler({}, {}))
